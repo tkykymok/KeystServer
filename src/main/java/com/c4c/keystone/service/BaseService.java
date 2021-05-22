24f4fc795650ab843d3e4b77bab1eacb350379ef
takayukiyamaoka <tkykymok@me.com>
2021-05-22 10:16:25 +0900

    Serviceクラスのinterfaceを修正した

---
+++ src/main/java/com/c4c/keystone/service/BaseService.java
@@ -0,0 +1,19 @@
+package com.c4c.keystone.service;
+
+import java.util.List;
+
+public interface BaseService<E, S, U>  {
+    /**
+     * PKによる検索を行います。
+     * @param EntityKey
+     * @return
+     */
+    E selectByPk(S EntityKey);
+
+    /**
+     * 任意の条件による検索を行います。
+     * @param EntityExample
+     * @return
+     */
+    List<E> select(U EntityExample);
+}
--- src/main/java/com/c4c/keystone/service/IKeyst0200Service.java
+++ src/main/java/com/c4c/keystone/service/IKeyst0200Service.java
@@ -4,10 +4,6 @@ import com.c4c.keystone.entity.Keyst0200;
 import com.c4c.keystone.entity.Keyst0200Example;
 import com.c4c.keystone.entity.Keyst0200Key;
 
-import java.util.List;
+public interface IKeyst0200Service extends BaseService<Keyst0200, Keyst0200Key, Keyst0200Example> {
 
-public interface IKeyst0200Service {
-    Keyst0200 selectByPk(Keyst0200Key keyst0200Key);
-
-    List<Keyst0200> select(Keyst0200Example keyst0200Example);
 }
