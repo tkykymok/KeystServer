be04b9d3a3ba6b7a40696591cc5ecb55af4f0a92
takayukiyamaoka <tkykymok@me.com>
2021-06-15 19:23:18 +0900

    スキルシート登録日時のformatを定義した

--- src/main/java/com/c4c/keystone/form/Keyst10200InitS04.java
+++ src/main/java/com/c4c/keystone/form/Keyst10200InitS04.java
@@ -1,5 +1,6 @@
 package com.c4c.keystone.form;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -16,5 +17,6 @@ public class Keyst10200InitS04 {
      * Column: SKILL_SHEET_REG_DATETIME
      * Remark: スキルシート登録日時
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime skillSheetRegDatetime;
 }
