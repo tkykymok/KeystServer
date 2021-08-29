43de0082bf6046c9766344e439cae4d6b34dcd7b
momokoeda <m.eda@c4c.co.jp>
2021-08-29 15:36:28 +0900

    add skills

--- src/main/java/com/c4c/keystone/controller/Keyst10100Controller.java
+++ src/main/java/com/c4c/keystone/controller/Keyst10100Controller.java
@@ -1,7 +1,9 @@
 package com.c4c.keystone.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.c4c.keystone.entity.Keyst5300;
 import com.c4c.keystone.form.Keyst10100InitS;
+import com.c4c.keystone.form.Keyst10400InitS01;
 import com.c4c.keystone.service.impl.Keyst10100Service;
 import com.c4c.keystone.service.impl.Keyst5300Service;
 import com.c4c.keystone.utils.JwtUtil;
@@ -37,21 +40,30 @@ public class Keyst10100Controller {
         // スキルマスタ取得
         List<Keyst5300> keyst5300List = keyst5300Service.getAllSkills();
 
+        // スキルリストForm(initS01List)を定義する。
+        List<Keyst10400InitS01> initS01List = new ArrayList<>();
+
         // カンマ区切りのスキルを一文字ずつリストに入れる
         String[] skillList = resForm.getUserBasicInfo().getSkills().split(",");
 
         // スキルコードを1件ずつ取り出し、スキル名に変換する。
         if (skillList != null) {
-            for (String skill : skillList) {
-                Integer skillCode = Integer.parseInt(skill);
-                System.out.println(skillCode);
-                for (int i = 0; i <= keyst5300List.size(); i++) {
-                    System.out.println(keyst5300List);
-                }
-            }
-        }
+            for (String skillCodeString : skillList) {
+                Keyst10400InitS01 initS01 = new Keyst10400InitS01();
+                Integer skillCode = Integer.parseInt(skillCodeString);
+                Keyst5300 keyst5300 = keyst5300List.stream()
+                        .filter(obj -> skillCode.equals(obj.getSkillCode()))
+                        .findFirst()
+                        .get();
 
+                // コード値からスキル名に変更した値をスキルForm(initS01)にコピーし、スキルリストForm(initS01List)に設定する。
+                BeanUtils.copyProperties(keyst5300, initS01);
+                initS01List.add(initS01);
+            }
+            // スキルリストForm(initS01List)をレスポンスFormに設定する。
+            resForm.setSkillList(initS01List);
+        }
         return ResponseEntity.ok(resForm);
     }
-
 }
+
