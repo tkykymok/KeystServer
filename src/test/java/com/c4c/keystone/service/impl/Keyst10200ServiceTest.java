e7d978ef23063558232fb045687cc553ebdf5966
takayukiyamaoka <tkykymok@me.com>
2021-10-07 14:51:15 +0900

    Junit検証(作業中)

---
+++ src/test/java/com/c4c/keystone/service/impl/Keyst10200ServiceTest.java
@@ -0,0 +1,28 @@
+package com.c4c.keystone.service.impl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Keyst10200ServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Nested
+    class initializeTest {
+        @Test
+        void test_001() {
+            System.out.println("検証");
+        }
+    }
+}
+
